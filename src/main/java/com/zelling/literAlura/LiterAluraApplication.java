51e01d43737f0b51f1c398dfc60dc97048fce6a8
FrancyelZelling <zellingf@gmail.com>
2025-01-02 07:55:13 -0300

    project started

---
+++ src/main/java/com/zelling/literAlura/LiterAluraApplication.java
@@ -0,0 +1,20 @@
+package com.zelling.literAlura;
+
+import com.zelling.literAlura.main.Application;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Application application = new Application();
+		application.app();
+	}
+}
---
+++ src/main/java/com/zelling/literAlura/main/Application.java
@@ -0,0 +1,7 @@
+package com.zelling.literAlura.main;
+
+public class Application {
+    public void app(){
+        System.out.println("app running");
+    }
+}
